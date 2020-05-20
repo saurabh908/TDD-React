55ec71c63576465ccebcdddfb8077ee8698e5230
Saurabh Kumar <usersaurabh92@gmail.com>
2020-05-20 19:22:56 +0530

    :work: Fixed prettier eslint with $PWD

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,15 @@
 module.exports = {
-  extends: ['eslint:recommended'],
+  extends: ['eslint:recommended', 'prettier'],
+  plugins: ['prettier'],
   parserOptions: {
     ecmaVersion: 6,
   },
   env:{
     node: true,
+  },
+  rules:{
+    quotes:['error','single',{
+      avoidEscape:true
+    }],
   },
 }
--- tests/palindromes.test.js
+++ tests/palindromes.test.js
@@ -3,5 +3,8 @@ const palindromes = require('../palindromes')
 describe('palindromes()', () => {
     it('correctly identifies one word palindrome', () => {
         expect(palindromes('madam')).toEqual(['madam']);
+        expect(palindromes('racecar')).toEqual(['racecar']);
     })
 })
+
+//npx prettier-eslint $PWD/tests/palindromes.test.js

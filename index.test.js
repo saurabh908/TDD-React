3ade04c028caa70534089c54151a5901fd90394a
Saurabh Kumar <usersaurabh92@gmail.com>
2020-05-20 14:06:06 +0530

    first commit1

--- index.test.js
+++ index.test.js
@@ -6,6 +6,16 @@ describe('fizzBuzz()', () => {
         expect(fizzBuzz(30)).toBe('FizzBuzz');
     })
 
+    it('returns "Fizz" for multiples of 3', () => {
+        expect(fizzBuzz(9)).toBe('Fizz');
+        expect(fizzBuzz(27)).toBe('Fizz');
+    })
+
+    it('returns "Buzz" for multiples of 5', () => {
+        expect(fizzBuzz(20)).toBe('Buzz');
+        expect(fizzBuzz(50)).toBe('Buzz');
+    })
+
     it('returns the given number for multiples of neither 3 nor 5', () => {
         expect(fizzBuzz(1)).toBe('1');
         expect(fizzBuzz(22)).toBe('22');

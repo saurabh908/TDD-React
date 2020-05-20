aa0fd52d4196844822f08c57bb819660a3bbbf2f
Saurabh Kumar <usersaurabh92@gmail.com>
2020-05-21 01:17:08 +0530

    :wrench: Installed eslint-plugin-react

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,28 @@
 module.exports = {
-  extends: ['eslint:recommended', 'plugin:prettier/recommended'],
-  plugins: ['prettier'],
+  plugins: ['prettier', 'react'],
+  extends: [
+    'eslint:recommended',
+    'plugin:prettier/recommended',
+    'plugin:react/recommended',
+  ],
   parserOptions: {
     ecmaVersion: 6,
     sourceType: 'module',
     ecmaFeatures: {
-      jsx: true
-    }
+      jsx: true,
+    },
   },
   env: {
-    node: true
+    node: true,
   },
   rules: {
     quotes: ['error', 'single', { avoidEscape: true }],
     'comma-dangle': ['error', 'always-multiline'],
-    'prettier/prettier': 'error'
-  }
+    'prettier/prettier': 'error',
+  },
+  settings: {
+    react: {
+      version: '16.13.1',
+    },
+  },
 };

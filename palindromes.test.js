abb57e09ff72530b293cf663c03917353a7c8d00
Saurabh Kumar <usersaurabh92@gmail.com>
2020-05-20 17:58:39 +0530

    :tad: First Commit

---
+++ palindromes.test.js
@@ -0,0 +1,7 @@
+const palindromes = require('./palindromes')
+
+describe('palindromes()', () => {
+    it('correctly identifies one word palindrome', () => {
+        expect(palindromes('madam')).toEqual(['madam']);
+    })
+})
